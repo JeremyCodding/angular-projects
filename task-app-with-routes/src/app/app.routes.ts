e0b0c22ecfec3017a713da1a080a981b1d7b2e9d
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-20 14:10:03 -0300

    Routing Task App: Working with canDeactivate route option

--- task-app-with-routes/src/app/app.routes.ts
+++ task-app-with-routes/src/app/app.routes.ts
@@ -1,16 +1,28 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
+  resolveTitle,
   resolveUserName,
   UserTasksComponent,
 } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { userRoutes } from './users/users.routes';
+import { inject } from '@angular/core';
+
+// const dummyCanMatch: CanMatchFn = (route, segments) => {
+//   const router = inject(Router);
+//   const shoudlGetAccess = Math.random();
+//   if (shoudlGetAccess < 0.5) {
+//     return true;
+//   }
+//   return new RedirectCommand(router.parseUrl('/unauthorized'));
+// };
 
 export const routes: Routes = [
   {
     path: '',
     component: NoTaskComponent,
+    title: 'No task selected',
   },
   {
     path: 'users/:userId',
@@ -22,6 +34,7 @@ export const routes: Routes = [
     resolve: {
       userName: resolveUserName,
     },
+    title: resolveTitle,
   },
   {
     path: '**',
