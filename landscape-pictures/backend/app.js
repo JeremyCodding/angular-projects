897dae491c40aadd3e7908fbb9606d2bbc75eee4
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-22 09:48:45 -0300

    Landscape Drawings App: Handling fetching wait and error in requests

--- landscape-pictures/backend/app.js
+++ landscape-pictures/backend/app.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/places", async (req, res) => {
+  // return res.status(500).json();
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
   const fileContent = await fs.readFile("./data/places.json");
